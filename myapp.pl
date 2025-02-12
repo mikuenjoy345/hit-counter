a3c91137dae0b73ab3ad6d3f43e29eec3aa31a7d
jjakke <jjakke@dev.jjakke.com>
2025-02-12 17:27:56 -0500

    add filelocking, merge write/read_counter subroutines

--- myapp.pl
+++ myapp.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite -signatures;
+use Fcntl ':flock';
+## no critic (prototypes)
 
 my $counter_file = 'counter.numb';
 my $counter = 0;
@@ -7,32 +9,41 @@ my $number_length = 6;
 
 app->hook(before_server_start => sub ($server, $app) {
 	if (! (-e $counter_file)) {
-		write_counter();
+		open my $fh, '>', $counter_file; # TODO:  what if opening fails?
+		flock($fh, LOCK_EX);
+		syswrite $fh, $counter, length $counter, 0;
+		close $fh;
 	}
-	read_counter();
+	else {
+		open my $fh, '<', $counter_file; # TODO:  what if opening fails?
+		flock($fh, LOCK_EX);
+		sysread $fh, $counter, 20; # 20 bytes to read which is more than enough for a counter
+		close $fh;
+	}
 	
 	-e '/bin/montage' or die 'ImageMagick not installed? `/bin/montage`';
 });
 
 get '/' => sub ($c) {
-	read_counter(); # hypnotoad or w/e may run on threads or something. awkward.
-	make_image(to_number_length($counter++));
+	update_counter();
+	make_image(to_number_length($counter));
 	$c->res->headers->header('Content-Security-Policy' => 'img-src * artemis.venus.place');
 	$c->res->headers->header('Server' => 'nginx/1.22.1'); # lie :)
 	$c->reply->file('tmp/counter.png');
-	write_counter();
 };
 
-sub write_counter () {
-	open my $fh, '>', $counter_file;
-	syswrite $fh, $counter, length $counter, 0;
-	close $fh;
-}
-
-sub read_counter () {
-	open my $fh, '<', $counter_file;
-	sysread $fh, $counter, 20; # 20 bytes to read which is more than enough for a counter
-	close $fh;
+sub update_counter () {
+	open my $fh, '+<', $counter_file; # TODO:  what if opening fails?
+	flock($fh, LOCK_EX);
+	sysread ($fh, $counter, 20) or warn "sysread error? $@ $!";
+	$counter++;
+	seek $fh, 0, 0;
+	syswrite ($fh, $counter, length $counter, 0) or warn "syswrite error? $@ $!";
+	my $res = close $fh;
+	if (! $res) {
+		print "close error (update) on $counter_file:  $!\n";
+		# TODO: actually do something about it.
+	}
 }
 
 sub to_number_length ($counter) {
